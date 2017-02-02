27d8f32cf0ae7a02badc1a580c7e805f1afe7c0a
Trixie Roque <troque@lion.lmu.edu>
2017-02-02 14:39:06 -0800

    Initial Xcode setup

---
+++ AppArel/AppArel/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AppArel
+//
+//  Created by Trixie on 2/2/17.
+//  Copyright © 2017 Blue Rook. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AppArel/AppArel/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AppArel
+//
+//  Created by Trixie on 2/2/17.
+//  Copyright © 2017 Blue Rook. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
